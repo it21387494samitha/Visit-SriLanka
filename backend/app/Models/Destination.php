8f15995f6b35ac80a5fccc3431a1b3c6c319d1bd
SamithaDhananjaya <dhananjayasamitha68@gmail.com>
2026-02-25 15:21:59 +0530

    Backend model added

---
+++ backend/app/Models/Destination.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    public function category()
+{
+    return $this->belongsTo(Category::class);
+}
+
+public function district()
+{
+    return $this->belongsTo(District::class);
+}
+
+public function images()
+{
+    return $this->hasMany(DestinationImage::class);
+}
+
+public function coverImage()
+{
+    return $this->hasOne(DestinationImage::class)->where('is_cover', true);
+}
+}
+
+
+
+
---
+++ backend/app/Models/District.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class District extends Model
+{
+
+
+public function destinations()
+{
+    return $this->hasMany(Destination::class);
+}
+
+
+}
